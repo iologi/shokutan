bf5a33eac5943e05aa3998634560e90468e57b64
iologi <root-ino@ymail.ne.jp>
2026-04-29 22:42:33 +0900

    add: 「食べたい」食材一覧ページ実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -6,4 +6,8 @@ class FoodsController < ApplicationController
   def show
     @food = Food.find(params[:id])
   end
+
+  def wishlist_foods
+    @foods = current_user.wishlisted_foods.includes(:category).order(created_at: :desc)
+  end
 end
