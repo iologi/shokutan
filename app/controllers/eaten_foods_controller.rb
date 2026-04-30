811c570b9df5fa5757e8bb1cd56ec65817dfdf00
iologi <root-ino@ymail.ne.jp>
2026-04-30 22:56:56 +0900

    add: 「食べた」機能のアクションを実装

--- app/controllers/eaten_foods_controller.rb
+++ app/controllers/eaten_foods_controller.rb
@@ -3,4 +3,19 @@ class EatenFoodsController < ApplicationController
     @food = Food.find(params[:food_id])
     @eaten_food = current_user.eaten_foods.new(food: @food)
   end
+
+  def create
+    @eaten_food = current_user.eaten_foods.new(eaten_food_params)
+    if @eaten_food.save
+      redirect_to "#"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def eaten_food_params
+    params.require(:eaten_food).permit(:food_id, :ate_on)
+  end
 end
