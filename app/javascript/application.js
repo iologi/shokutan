9c80b6b49350ad9dd5f0fc20fe36da1759459b67
iologi <root-ino@ymail.ne.jp>
2026-05-02 21:59:18 +0900

    add: raty-jsの設定

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import Raty from "./raty"
+
+// ratyアクションの定義
+window.raty = function(elem,opt){ 
+    let raty =  new Raty(elem,opt)
+    raty.init();
+    return raty;
+}
