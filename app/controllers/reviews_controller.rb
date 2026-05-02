a6ee8fd15dada7d2820c3573f5c8c6646e6a5412
iologi <root-ino@ymail.ne.jp>
2026-05-02 17:03:09 +0900

    add: レビュー投稿機能のアクションを実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,20 @@ class ReviewsController < ApplicationController
     @eaten_food = current_user.eaten_foods.find(params[:eaten_food_id])
     @review = @eaten_food.build_review
   end
+
+  def create
+    @eaten_food = current_user.eaten_foods.find(params[:eaten_food_id])
+    @review = @eaten_food.build_review(review_params)
+    if @review.save
+      redirect_to food_path(@eaten_food.food)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:satisfaction, :comment)
+  end
 end
